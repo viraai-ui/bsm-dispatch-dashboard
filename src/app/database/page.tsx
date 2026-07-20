79a82b36ac6a82eddccc95d136fec6d850c83d8f
viraaiagent-6679 <272841182+viraai-ui@users.noreply.github.com>
2026-07-20 17:43:28 +0530

    Polish mobile database layout

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -8,5 +8,5 @@ export const dynamic = 'force-dynamic'
 export default async function DatabasePage() {
   const orders = await listSyncedOrders()
   const { statuses, mediaRecords } = await buildOrderStatusMap(orders)
-  return <DashboardShell active="Database"><header className="top compact-top"><div><h1 className="h1">Database</h1></div></header><DatabaseClient orders={orders} mediaRecords={mediaRecords} statuses={statuses} /></DashboardShell>
+  return <DashboardShell active="Database"><DatabaseClient orders={orders} mediaRecords={mediaRecords} statuses={statuses} /></DashboardShell>
 }
