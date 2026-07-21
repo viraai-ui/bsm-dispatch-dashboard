adb9f8f33d268c4c6bd19e4c9a1aa40693e2a1e3
viraaiagent-6679 <272841182+viraai-ui@users.noreply.github.com>
2026-07-21 12:51:27 +0530

    Filter database to workflow orders

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -2,11 +2,15 @@ import { DashboardShell } from '@/components/DashboardShell'
 import { DatabaseClient } from '@/components/DatabaseClient'
 import { listSyncedOrders } from '@/lib/synced-orders'
 import { buildOrderStatusMap } from '@/lib/status-projection'
+import { listWorkflows } from '@/lib/workflow-store'
 
 export const dynamic = 'force-dynamic'
 
 export default async function DatabasePage() {
   const orders = await listSyncedOrders()
-  const { statuses, mediaRecords } = await buildOrderStatusMap(orders)
-  return <DashboardShell active="Database"><DatabaseClient orders={orders} mediaRecords={mediaRecords} statuses={statuses} /></DashboardShell>
+  const workflows = await listWorkflows()
+  const workflowOrderIds = new Set(Object.keys(workflows))
+  const databaseOrders = orders.filter((order) => workflowOrderIds.has(order.id))
+  const { statuses, mediaRecords } = await buildOrderStatusMap(databaseOrders, workflows)
+  return <DashboardShell active="Database"><DatabaseClient orders={databaseOrders} mediaRecords={mediaRecords} statuses={statuses} /></DashboardShell>
 }
